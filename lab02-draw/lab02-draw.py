b04230a22f77408e2f4242ac7b7295dd2e561648
ikm24 <iker.manzano@edu.uah.es>
2023-02-16 16:37:58 +0100

    Montañas mas bonitas

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -14,10 +14,15 @@ arcade.set_background_color(arcade.color.ATOMIC_TANGERINE)
 arcade.start_render()
 
 
-
-#Montaña
-arcade.draw_triangle_filled(300,100,600,700,900,100,arcade.color.COOL_GREY)
+#Montaña centro
+arcade.draw_triangle_filled(100,200,400,800,700,200,arcade.color.AUROMETALSAURUS)
+arcade.draw_triangle_filled(350,700,400,800,450,700,arcade.color.WHITE)
+#Montaña derecha
+arcade.draw_triangle_filled(300,100,600,700,900,100,arcade.color.ASH_GREY)
 arcade.draw_triangle_filled(550,600,600,700,650,600,arcade.color.WHITE)
+#Montaña izquierda
+arcade.draw_triangle_filled(-100,100,200,700,500,100,arcade.color.COOL_GREY)
+arcade.draw_triangle_filled(150,600,200,700,250,600,arcade.color.WHITE)
 
 
 
