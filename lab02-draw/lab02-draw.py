5b5f94e9e066ac66d5b1fbd73b4c592a60401ad7
ikm24 <iker.manzano@edu.uah.es>
2023-02-16 15:49:41 +0100

    Ventana y final creados

---
+++ lab02-draw/lab02-draw.py
@@ -0,0 +1,16 @@
+"""
+Dibujo laaboratiorio (Ejercicio 2)
+"""
+
+import arcade
+
+#Plantilla de dibujo
+arcade.open_window(1200,800,"Mi precioso dibujo")
+
+
+
+
+#Mantener pestaña abierta
+arcade.run()
+
+
