474b0be760349520673d756a8dc78899740d77b5
ikm24 <iker.manzano@edu.uah.es>
2023-02-16 16:23:50 +0100

    DIBUJABLE

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -5,16 +5,21 @@ Dibujo laaboratiorio (Ejercicio 2)
 import arcade
 
 #Plantilla de dibujo
-arcade.open_window(800,600,"Mi precioso dibujo")
+arcade.open_window(1200,800,"Mi precioso dibujo")
 
 #Fondo
 arcade.set_background_color(arcade.color.ATOMIC_TANGERINE)
 
-#Comienzo para dibujar (?)
+#Comienzo para dibujar
 arcade.start_render()
 
+#Nubes (?)
+
+arcade.draw_texture_rectangle(600,500,800,200,arcade.texture.Texture)
+arcade.draw_texture_rectangle()
+
 #Montaña
-arcade.draw_triangle_filled(300,100,600,400,900,100,arcade.color.COOL_GREY)
+arcade.draw_triangle_filled(300,100,600,700,900,100,arcade.color.COOL_GREY)
 
 
 #Acabar dibujo
