b998e7865ea76a10b723d7cba3fe866311871bc8
Ikm24 <iker.manzano@edu.uah.es>
2023-02-17 13:45:24 +0100

    Lago y árbol bien hecho :D

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -7,12 +7,16 @@ import arcade
 #Plantilla de dibujo
 arcade.open_window(1200,800,"Mi precioso dibujo")
 
-#Fondo
-arcade.set_background_color(arcade.color.BABY_BLUE)
+#Fondo (forma el lago)
+arcade.set_background_color(arcade.color.BLEU_DE_FRANCE)
+
+
 
 #Comienzo para dibujar
 arcade.start_render()
 
+#Cielo
+arcade.draw_rectangle_filled(600,500,1200,800,arcade.color.BABY_BLUE)
 
 #Montaña centro
 arcade.draw_triangle_filled(100,200,400,800,700,200,arcade.color.AUROMETALSAURUS)
@@ -28,7 +32,7 @@ arcade.draw_triangle_filled(150,600,200,700,250,600,arcade.color.GHOST_WHITE)
 arcade.draw_circle_filled(1400,-500,1000,arcade.color.DOLLAR_BILL)
 
 #Arboles
-arcade.draw_rectangle_filled(725,200,50,250,arcade.color.DONKEY_BROWN)
+arcade.draw_rectangle_filled(725,200,10,50,arcade.color.DONKEY_BROWN)
 arcade.draw_triangle_filled(700,250,725,300,750,250,arcade.color.AO)
 arcade.draw_triangle_filled(700,225,725,275,750,225,arcade.color.AO)
 arcade.draw_triangle_filled(700,200,725,250,750,200,arcade.color.AO)
