b99e9af40ab6524d047589525c7b3921cfc6bc6e
ikm24 <iker.manzano@edu.uah.es>
2023-02-23 15:59:18 +0100

    Comienzo escala arbol

--- lab02-draw/lab02-draw2.py
+++ lab02-draw/lab02-draw2.py
@@ -4,6 +4,16 @@ Dibujo lab usando funciones
 
 import arcade
 
+#Declaracion de funciones
+def arbol(x,y,e=1):
+    '''int, int, int  --> nada
+    OBJ: crea un arbol con la base del tronco en las cordenadas x e y,
+    de tamaño en relacion con el escalar e'''
+    arcade.draw_rectangle_filled(x, y+25, 10, 50, arcade.color.DONKEY_BROWN)
+    arcade.draw_triangle_filled(x-25, y+75, x, y+125, x+25, y+75, arcade.color.AO)
+    arcade.draw_triangle_filled(x-25, y+50, x, y+100, x+25, y+50, arcade.color.AO)
+    arcade.draw_triangle_filled(x-25, y+25, x, y+75, x+25, y+25, arcade.color.AO)
+
 #Plantilla de dibujo
 arcade.open_window(1200,800,"Mi precioso dibujo")
 
@@ -41,13 +51,7 @@ arcade.draw_rectangle_filled(600,200,1200,200,arcade.color.BLEU_DE_FRANCE)
 arcade.draw_circle_filled(1400,-500,1000,arcade.color.DOLLAR_BILL)
 '''
 
-def arbol(x,y):
-    '''int, int --> nada
-    OBJ: crea un arbol con la base del tronco en las cordenadas x e y'''
-    arcade.draw_rectangle_filled(x, y+25, 10, 50, arcade.color.DONKEY_BROWN)
-    arcade.draw_triangle_filled(x-25, y+75, x, y+125, x+25, y+75, arcade.color.AO)
-    arcade.draw_triangle_filled(x-25, y+50, x, y+100, x+25, y+50, arcade.color.AO)
-    arcade.draw_triangle_filled(x-25, y+25, x, y+75, x+25, y+25, arcade.color.AO)
+
 
 '''
 #Arboles
@@ -57,13 +61,17 @@ arcade.draw_triangle_filled(700,225,725,275,750,225,arcade.color.AO)
 arcade.draw_triangle_filled(700,200,725,250,750,200,arcade.color.AO)
 '''
 
+#"Cuadricula" temporal
 arcade.draw_rectangle_filled(600,400,800,1,arcade.color.ALABAMA_CRIMSON)
 arcade.draw_rectangle_filled(600,375,800,1,arcade.color.ALABAMA_CRIMSON)
 arcade.draw_rectangle_filled(600,350,800,1,arcade.color.ALABAMA_CRIMSON)
 arcade.draw_rectangle_filled(600,325,800,1,arcade.color.ALABAMA_CRIMSON)
 
-arbol(600,400)
+arcade.draw_triangle_filled(700,250,725,300,750,250,arcade.color.AO)
 
+'''
+arbol(800,400)
+'''
 
 #Acabar dibujo
 arcade.finish_render()
