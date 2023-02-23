5d386e43d698a3e86e3d01412a0614223b528569
ikm24 <iker.manzano@edu.uah.es>
2023-02-23 15:51:30 +0100

    El arbol esta increible

--- lab02-draw/lab02-draw2.py
+++ lab02-draw/lab02-draw2.py
@@ -8,7 +8,7 @@ import arcade
 arcade.open_window(1200,800,"Mi precioso dibujo")
 
 #Fondo
-arcade.set_background_color(arcade.color.BABY_BLUE)
+arcade.set_background_color(arcade.color.WHITE)
 
 
 #Comienzo para dibujar
@@ -43,11 +43,11 @@ arcade.draw_circle_filled(1400,-500,1000,arcade.color.DOLLAR_BILL)
 
 def arbol(x,y):
     '''int, int --> nada
-    OBJ: crea un arbol con centro en las cordenadas x e y'''
-    arcade.draw_rectangle_filled(x, y-50, 10, 50, arcade.color.DONKEY_BROWN)
-    arcade.draw_triangle_filled(x-25, y, x, y+50, x+25, y, arcade.color.AO)
-    arcade.draw_triangle_filled(x-25, y-25, x, y+25, x+25, y-25, arcade.color.AO)
-    arcade.draw_triangle_filled(x-25, y-50, x, y, x+25, y-50, arcade.color.AO)
+    OBJ: crea un arbol con la base del tronco en las cordenadas x e y'''
+    arcade.draw_rectangle_filled(x, y+25, 10, 50, arcade.color.DONKEY_BROWN)
+    arcade.draw_triangle_filled(x-25, y+75, x, y+125, x+25, y+75, arcade.color.AO)
+    arcade.draw_triangle_filled(x-25, y+50, x, y+100, x+25, y+50, arcade.color.AO)
+    arcade.draw_triangle_filled(x-25, y+25, x, y+75, x+25, y+25, arcade.color.AO)
 
 '''
 #Arboles
@@ -56,6 +56,14 @@ arcade.draw_triangle_filled(700,250,725,300,750,250,arcade.color.AO)
 arcade.draw_triangle_filled(700,225,725,275,750,225,arcade.color.AO)
 arcade.draw_triangle_filled(700,200,725,250,750,200,arcade.color.AO)
 '''
+
+arcade.draw_rectangle_filled(600,400,800,1,arcade.color.ALABAMA_CRIMSON)
+arcade.draw_rectangle_filled(600,375,800,1,arcade.color.ALABAMA_CRIMSON)
+arcade.draw_rectangle_filled(600,350,800,1,arcade.color.ALABAMA_CRIMSON)
+arcade.draw_rectangle_filled(600,325,800,1,arcade.color.ALABAMA_CRIMSON)
+
+arbol(600,400)
+
 
 #Acabar dibujo
 arcade.finish_render()
