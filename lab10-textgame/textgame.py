df4e3bd14d711a85b47779fb264a420fb6e64508
ikm24 <iker.manzano@edu.uah.es>
2023-03-02 15:50:45 +0100

    Class Room and main() created

--- lab10-textgame/textgame.py
+++ lab10-textgame/textgame.py
@@ -1,3 +1,19 @@
 """
-Juego de texto
+Text game
 """
+
+class Room:
+    """Represents a room"""
+
+    def __init__(self, description, north, south, east, west):
+        self.description = description
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+
+def main():
+    room_list = []
+    room = Room("Lobby",None,None,1,3)
+
+
