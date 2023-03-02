90cb0ed74d051e1690383f327b1d65af3f9975e9
ikm24 <iker.manzano@edu.uah.es>
2023-03-02 16:02:32 +0100

    All rooms created

--- lab10-textgame/textgame.py
+++ lab10-textgame/textgame.py
@@ -14,6 +14,14 @@ class Room:
 
 def main():
     room_list = []
-    room = Room("Lobby",None,None,1,3)
+    Lobby = Room("Lobby", None, None, 3, 1)
+    Dining_room = Room("Dining room", 2, None, 0, None)
+    Kitchen = Room("Kitchen", None, 1, None, None)
+    Lounge = Room("Lounge", 5, 0, 4, None)
+    Balcony = Room("Balcony", None, None, None, 3)
+    Hallway = Room("Hallway", 6, 7, 3, 8)
+    Bathroom = Room("Bathroom", 5, None, None, None)
+    Bedroom = Room("Bedroom", None, 5, None, None)
+    Attic = Room("Attic", None, None, 5, None)
 
-
+    room_list.append(Lobby)
