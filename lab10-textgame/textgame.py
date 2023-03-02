0b10ae1c2e799f96164a01abfb81e04ea4f5e4a6
ikm24 <iker.manzano@edu.uah.es>
2023-03-02 16:56:29 +0100

    Acabado?

--- lab10-textgame/textgame.py
+++ lab10-textgame/textgame.py
@@ -17,12 +17,13 @@ def main():
     SUR=["s","S","south","South","SOUTH","Sur","sur","SUR"]
     ESTE=["e","E","east","East","EAST","Este","este","ESTE"]
     OESTE=["w","W","o","O","west","West","WEST","Oeste","OESTE","oeste"]
+    QUIT=["q","Q","Quit","quit","QUIT","Esc","ESC","esc","adios","Adios","ADIOS","bye","Bye","BYE"]
 
     room_list = []
     lobby = Room(
         "Te despiertas en lo que parece la entrada de una casa. No sabes como has llegado alli y te sientes algo " +
         "mareado y desubicado.\nTratas de abrir la puerta principal de la casa, fallando en el intento: esta cerrada con llave. " +
-        "Decides explorar la casa en busca de otra salida.\nTienes dos puertas accesibles, al este(e) y al oeste(w)."
+        "Decides explorar la casa en busca de otra salida.\nTienes dos puertas accesibles, al este y al oeste."
                  , None, None, 3, 1)
     room_list.append(lobby)
     dining_room = Room("Dining room", 2, None, 0, None)
@@ -48,7 +49,31 @@ def main():
     while (done==False):
         print(room_list[current_room].description)
         print("\n")
-        input(print("Que desea hacer: "))
-        print("\n")
+        answer= input(print("Que desea hacer: "))
+        if (answer in NORTE):
+            try:
+                next_room=(room_list[current_room].noth)
+            except:
+                print("Te estampas contra la pared, pensando el por qué de tus acciones\n")
+        elif (answer in SUR):
+            try:
+                next_room = (room_list[current_room].south)
+            except:
+                print("Te estampas contra la pared, pensando el por qué de tus acciones\n")
+        elif (answer in ESTE):
+            try:
+                next_room = (room_list[current_room].east)
+            except:
+                print("Te estampas contra la pared, pensando el por qué de tus acciones\n")
+        elif (answer in OESTE):
+            try:
+                next_room = (room_list[current_room].west)
+            except:
+                print("Te estampas contra la pared, pensando el por qué de tus acciones\n")
+        elif (answer in QUIT):
+            done=True
+        else:
+            print("Eso no tiene pinta de direccion\n")
+
 
 main()
