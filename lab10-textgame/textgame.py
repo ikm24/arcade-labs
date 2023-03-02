2c19fab2e83f48baef1624caceaf407b84db876f
ikm24 <iker.manzano@edu.uah.es>
2023-03-02 16:32:37 +0100

    primera descripcion, todo montado :)

--- lab10-textgame/textgame.py
+++ lab10-textgame/textgame.py
@@ -14,14 +14,32 @@ class Room:
 
 def main():
     room_list = []
-    Lobby = Room("Lobby", None, None, 3, 1)
-    Dining_room = Room("Dining room", 2, None, 0, None)
-    Kitchen = Room("Kitchen", None, 1, None, None)
-    Lounge = Room("Lounge", 5, 0, 4, None)
-    Balcony = Room("Balcony", None, None, None, 3)
-    Hallway = Room("Hallway", 6, 7, 3, 8)
-    Bathroom = Room("Bathroom", 5, None, None, None)
-    Bedroom = Room("Bedroom", None, 5, None, None)
-    Attic = Room("Attic", None, None, 5, None)
+    lobby = Room(
+        "Te despiertas en lo que parece la entrada de una casa. No sabes como has llegado alli y te sientes algo " +
+        "mareado y desubicado.\nTratas de abrir la puerta principal de la casa, fallando en el intento: esta cerrada con llave. " +
+        "Decides explorar la casa en busca de otra salida.\nTienes dos puertas accesibles, al este(e) y al oeste(w)."
+                 , None, None, 3, 1)
+    room_list.append(lobby)
+    dining_room = Room("Dining room", 2, None, 0, None)
+    room_list.append(dining_room)
+    kitchen = Room("Kitchen", None, 1, None, None)
+    room_list.append(kitchen)
+    lounge = Room("Lounge", 5, 0, 4, None)
+    room_list.append(lounge)
+    balcony = Room("Balcony", None, None, None, 3)
+    room_list.append(balcony)
+    hallway = Room("Hallway", 6, 7, 3, 8)
+    room_list.append(hallway)
+    bedroom = Room("Bedroom", None, 5, None, None)
+    room_list.append(bedroom)
+    bathroom = Room("Bathroom", 5, None, None, None)
+    room_list.append(bathroom)
+    attic = Room("Attic", None, None, 5, None)
+    room_list.append(attic)
+    current_room = 0
+    done = False
 
-    room_list.append(Lobby)
+    while (done==False):
+        print(room_list[current_room].description)
+
+main()
