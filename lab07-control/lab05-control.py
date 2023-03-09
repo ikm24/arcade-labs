613d1a76170af6d5bf6ecb9889a3d7adecf06652
ikm24 <iker.manzano@edu.uah.es>
2023-03-09 16:10:00 +0100

    Todo arreglado :D

--- lab07-control/lab05-control.py
+++ lab07-control/lab05-control.py
@@ -7,21 +7,19 @@ import random
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-class Arbol:
-    def __init__(self,position_x,postion_y):
+class Tree:
+    def __init__(self,position_x,position_y):
 
         self.position_x = position_x
         self.position_y = position_y
-    def arbol(x, y):
-        """int, int, int  --> nada
-        OBJ: crea un arbol con la base del tronco en las cordenadas x e y"""
-        arcade.draw_rectangle_filled(x, y + 25, 10, 50, arcade.color.DONKEY_BROWN)
-        arcade.draw_triangle_filled(x - 25, y + 75, x, y + 125, x + 25, y + 75, arcade.color.AO)
-        arcade.draw_triangle_filled(x - 25, y + 50, x, y + 100, x + 25, y + 50, arcade.color.AO)
-        arcade.draw_triangle_filled(x - 25, y + 25, x, y + 75, x + 25, y + 25, arcade.color.AO)
+
 
     def draw(self):
-        arbol(self.position_x,self.position_y)
+        arcade.draw_rectangle_filled(self.position_x, self.position_y + 25, 10, 50, arcade.color.DONKEY_BROWN)
+        arcade.draw_triangle_filled(self.position_x - 25, self.position_y + 75, self.position_x, self.position_y + 125, self.position_x + 25, self.position_y + 75, arcade.color.AO)
+        arcade.draw_triangle_filled(self.position_x - 25, self.position_y + 50, self.position_x, self.position_y + 100, self.position_x + 25, self.position_y + 50, arcade.color.AO)
+        arcade.draw_triangle_filled(self.position_x - 25, self.position_y + 25, self.position_x, self.position_y + 75, self.position_x + 25, self.position_y + 25, arcade.color.AO)
+
 class Ball:
     def __init__(self, position_x, position_y, radius, color):
 
@@ -50,12 +48,15 @@ class MyGame(arcade.Window):
 
         self.ball = Ball(50,50,15,arcade.color.FLORAL_WHITE)
 
+
+        self.tree = Tree(400,300)
+
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.DOLLAR_BILL)
 
         self.ball.draw()
-        # Arboles
+        self.tree.draw()
 
 
 
